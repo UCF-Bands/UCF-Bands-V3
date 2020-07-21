ed81adacc7bb1c0340b753e5787f58f13f979097
Jordan Pakrosnis <jpak@jordanpak.com>
2020-07-21 00:30:34 -0400

    Init featured-links template part

---
+++ wp-content/themes/wprig-ucf-bands/template-parts/header/featured-links.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template part for displaying the header's "featured links" (ex: Armory)
+ *
+ * @since   1.0.0
+ * @package wp_rig
+ */
+
+foreach ( [
+	'armory',
+] as $key ) {
+	$links[ $key ] = get_permalink( get_theme_mod( "ft_link_$key" ) );
+}
+
+// Make sure something came back.
+$links = array_filter( $links );
+
+if ( ! $links ) {
+	return;
+}
+?>
+
+<nav class="featured-links">
+
+	<?php if ( $links['armory'] ) : ?>
+		<a href="<?php echo esc_url( $links['armory'] ); ?>">
+			<?php esc_html_e( 'Armory', 'wp-rig' ); ?>
+			<i class="far fa-shield-alt"></i>
+		</a>
+	<?php endif; ?>
+
+</nav>
