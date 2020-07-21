db27a70022b99617709315c68feb015c4a0964b4
Jordan Pakrosnis <jpak@jordanpak.com>
2020-07-21 00:13:00 -0400

    Use get_theme_mod/parse_args functions in contact component

--- wp-content/themes/wprig-ucf-bands/inc/Contacts/Component.php
+++ wp-content/themes/wprig-ucf-bands/inc/Contacts/Component.php
@@ -14,6 +14,8 @@ use WP_Customize_Manager;
 use function WP_Rig\WP_Rig\wp_rig;
 use function add_action;
 use function add_filter;
+use function get_theme_mod;
+use function wp_parse_args;
 
 /**
  * Class for managing global contact information
