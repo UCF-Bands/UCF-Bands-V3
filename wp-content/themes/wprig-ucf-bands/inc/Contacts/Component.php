54754f5191459fe93869efce18108c3fd1d4ef52
Jordan Pakrosnis <jpak@jordanpak.com>
2020-07-20 23:58:17 -0400

    Add contact/address getter to component

--- wp-content/themes/wprig-ucf-bands/inc/Contacts/Component.php
+++ wp-content/themes/wprig-ucf-bands/inc/Contacts/Component.php
@@ -9,6 +9,7 @@
 namespace WP_Rig\WP_Rig\Contacts;
 
 use WP_Rig\WP_Rig\Component_Interface;
+use WP_Rig\WP_Rig\Templating_Component_Interface;
 use WP_Customize_Manager;
 use function WP_Rig\WP_Rig\wp_rig;
 use function add_action;
@@ -18,7 +19,7 @@ use function add_action;
  *
  * @since 1.0.0
  */
-class Component implements Component_Interface {
+class Component implements Component_Interface, Templating_Component_Interface {
 
 	/**
 	 * Customizer setting and control ID prefix
@@ -27,6 +28,14 @@ class Component implements Component_Interface {
 	 * @since 1.0.0
 	 */
 	private $prefix = 'contact';
+
+	/**
+	 * Theme mod cache
+	 *
+	 * @var   array
+	 * @since 1.0.0
+	 */
+	private $contacts = [];
 
 	/**
 	 * Gets the unique identifier for the theme component.
@@ -42,6 +51,23 @@ class Component implements Component_Interface {
 	 */
 	public function initialize() {
 		add_action( 'customize_register', [ $this, 'action_customize_register' ] );
+		add_filter( 'wp_rig_get_contact_phone', 'esc_attr' );
+		add_filter( 'wp_rig_get_contact_email', 'sanitize_email' );
+	}
+
+	/**
+	 * Gets template tags to expose as methods on the Template_Tags class instance, accessible through `wp_rig()`.
+	 *
+	 * @return array Associative array of $method_name => $callback_info pairs. Each $callback_info must either be
+	 *               a callable or an array with key 'callable'. This approach is used to reserve the possibility of
+	 *               adding support for further arguments in the future.
+	 */
+	public function template_tags() : array {
+		return [
+			'get_contact' => [ $this, 'get_contact' ],
+			'get_address' => [ $this, 'get_address' ],
+			'address'     => [ $this, 'do_address' ],
+		];
 	}
 
 	/**
@@ -108,5 +134,62 @@ class Component implements Component_Interface {
 				)
 			);
 		}
+	}
+
+	/**
+	 * Get a single piece of contact information (theme mod)
+	 *
+	 * @param  string $field Theme mod value to get.
+	 * @return string        Formatted field.
+	 *
+	 * @since  1.0.0
+	 */
+	public function get_contact( $field ) {
+
+		// Check cache.
+		if ( isset( $this->contacts[ $field ] ) ) {
+			return $this->contacts[ $field ];
+		}
+
+		$value = apply_filters(
+			"wp_rig_get_contact_$field",
+			get_theme_mod( "{$this->prefix}_$field" )
+		);
+
+		$this->contacts[ $field ] = $value;
+		return $value;
+	}
+
+	/**
+	 * Get the formatted address
+	 *
+	 * @return string
+	 * @since  1.0.0
+	 */
+	public function get_address() {
+
+		// Check cache.
+		if ( isset( $this->contacts['address'] ) ) {
+			return $this->contacts['address'];
+		}
+
+		// Get all the pieces.
+		for ( $line = 1; $line <= 4; $line++ ) {
+			$lines[ "addr_$line" ] = $this->get_contact( "addr_$line" );
+		}
+
+		// Filter out the empty ones and separate with <br>.
+		$lines = implode( '<br>', array_filter( $lines ) );
+
+		return "<address>$lines</address>";
+	}
+
+	/**
+	 * Output formatted address
+	 *
+	 * @since 1.0.0
+	 */
+	public function do_address() {
+		echo $this->get_address(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
