3208d918be65281ee0ee3fcd82247ecdb668fc18
Jordan Pakrosnis <jpak@jordanpak.com>
2020-08-22 18:40:13 -0400

    Add dynamic banner "addl" block attr placeholder

---
+++ wp-content/plugins/knight-blocks/src/blocks/dynamic-banner-addl/index.js
@@ -0,0 +1,12 @@
+// menu: {
+// 	type: 'number',
+// 	source: 'meta',
+// 	meta: '_dynamic_banner_menu',
+// },
+
+// // future taxonomy
+// ensemble: {
+// 	type: 'number',
+// 	source: 'meta',
+// 	meta: '_dynamic_banner_ensemble',
+// },
