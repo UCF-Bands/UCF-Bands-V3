866f66eb0ed0eecc734711b4e0fea91cc76f595f
Jordan Pakrosnis <jpak@jordanpak.com>
2020-08-31 21:47:09 -0400

    Actually add InnerBlocks in addl' save to kill weird paragraph throw "Update" button thing

--- wp-content/plugins/knight-blocks/src/blocks/dynamic-banner-addl/save.js
+++ wp-content/plugins/knight-blocks/src/blocks/dynamic-banner-addl/save.js
@@ -4,13 +4,12 @@
  * @since   1.0.0
  * @package Knight_Blocks
  */
-// const { InnerBlocks } = wp.blockEditor;
+const { InnerBlocks } = wp.blockEditor;
 
 export default function save( { className } ) {
-	return null;
-	// return (
-	// 	<header className={ className }>
-	// 		<InnerBlocks.Content />
-	// 	</header>
-	// );
+	return (
+		<div className={ className }>
+			<InnerBlocks.Content />
+		</div>
+	);
 }
