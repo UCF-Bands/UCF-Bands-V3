694228acd6154064bdb30c9716bc0672f43b1e35
Jordan Pakrosnis <jpak@jordanpak.com>
2020-10-09 17:21:57 -0400

    Add get_svg() helper

--- wp-content/plugins/knight-blocks/src/functions.php
+++ wp-content/plugins/knight-blocks/src/functions.php
@@ -34,3 +34,22 @@ function get_current_top_level_parent() {
 function get_can_user_edit_posts() {
 	return current_user_can( 'edit_posts' );
 }
+
+/**
+ * Get the contents of a SVG
+ *
+ * @param  string $filename Filename of SVG in /dist/images without .svg.
+ * @return string           Contents of SVG file if found.
+ *
+ * @since  1.0.0
+ */
+function get_svg( $filename ) {
+
+	$path = KNIGHT_BLOCKS_DIR . "/dist/images/$filename.svg";
+
+	if ( ! file_exists( $path ) ) {
+		return '';
+	}
+
+	return file_get_contents( $path ); // phpcs:disable
+}
