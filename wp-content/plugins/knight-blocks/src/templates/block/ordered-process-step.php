025da74a06e458da94761660a1e0f0645e3568e9
Jordan Pakrosnis <jpak@jordanpak.com>
2020-11-19 18:53:38 -0500

    Do more process step templating

--- wp-content/plugins/knight-blocks/src/templates/block/ordered-process-step.php
+++ wp-content/plugins/knight-blocks/src/templates/block/ordered-process-step.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Ordered process item dynamic block template
+ * Ordered process step dynamic block template
  *
  * @since   1.0.0
  * @package Knight_Blocks
@@ -8,12 +8,42 @@
 
 use function Knight_Blocks\get_allowed_inline_html;
 
-switch ( $type ) {
-	default:
-		break;
-}
+d( $status, $title, $description, $type, $url );
+
+$is_download = 'download' === $type;
 ?>
 
 <figure>
-	<h2><?php echo $title; ?></h2>
+
+	<div class="ordered-process-step-number">
+		<span class="ordered-process-step-status-circle">
+			<?php
+			/* translators: Step is %s */
+			printf( esc_html__( 'Step is %s', 'knight-blocks' ), esc_html( $status ) );
+			?>
+		</span>
+	</div>
+
+	<figcaption>
+		<?php if ( $title ) : ?>
+			<h4 class="h6 ordered-process-step-title">
+				<?php if ( $url ) : ?>
+					<a
+						href="<?php echo esc_url( $url ); ?>"
+						<?php echo esc_attr( $is_download ? 'download' : '' ); ?>
+					>
+				<?php endif; ?>
+
+				<?php echo esc_html( $title ); ?>
+
+				<?php if ( $url ) : ?>
+					<i class="far fa-<?php echo esc_html( $is_download ? 'download' : 'long-arrow-alt-right' ); ?>"></i></a>
+				<?php endif; ?>
+			</h4>
+		<?php endif; ?>
+
+		<?php if ( $description ) : ?>
+			<p><?php echo wp_kses( $description, get_allowed_inline_html() ); ?></p>
+		<?php endif; ?>
+	</figcaption>
 </figure>
