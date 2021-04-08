11a374fca08da86919ca7f25f8647255c5e4b7ee
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-07 23:55:34 -0400

    Add page post type-specific stylesheet for big entry-content first/last margins and increased max content width

--- wp-content/themes/wprig-ucf-bands/inc/Styles/Component.php
+++ wp-content/themes/wprig-ucf-bands/inc/Styles/Component.php
@@ -340,6 +340,10 @@ class Component implements Component_Interface, Templating_Component_Interface {
 					return wp_rig()->is_primary_sidebar_active();
 				},
 			],
+			'wp-rig-page'       => [
+				'file'             => 'page.min.css',
+				'preload_callback' => '__return_true',
+			],
 			'wp-rig-front-page' => [
 				'file' => 'front-page.min.css',
 				'preload_callback' => function() {
--- wp-content/themes/wprig-ucf-bands/index.php
+++ wp-content/themes/wprig-ucf-bands/index.php
@@ -17,7 +17,11 @@ namespace WP_Rig\WP_Rig;
 
 get_header();
 
-wp_rig()->print_styles( 'wp-rig-content' );
+if ( is_page() ) {
+	wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-page' );
+} else {
+	wp_rig()->print_styles( 'wp-rig-content' );
+}
 
 ?>
 	<main id="primary" class="site-main">
