1fef0d3352fddf8974df45100a91e084d3f68848
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-09 13:08:32 -0400

    Remove page-specific entry_header since we still want something in search archives and stuff

--- wp-content/themes/wprig-ucf-bands/template-parts/content/entry_header-page.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template part for displaying a page's header
- *
- * @since   3.0.0
- * @package wp_rig
- */
-
-namespace WP_Rig\WP_Rig;
--- wp-content/themes/wprig-ucf-bands/template-parts/content/entry_loop.php
+++ wp-content/themes/wprig-ucf-bands/template-parts/content/entry_loop.php
@@ -11,7 +11,6 @@ namespace WP_Rig\WP_Rig;
 $this_post_type = get_post_type();
 ?>
 
-
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
 	<?php
 	get_template_part( 'template-parts/content/entry_header', $this_post_type );
