3c6f451c339b2daee1dc1c1c63e02c6940d7aff3
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-11 18:11:29 -0400

    Add loop-entry class to loop entry template

--- wp-content/themes/wprig-ucf-bands/template-parts/content/entry_loop.php
+++ wp-content/themes/wprig-ucf-bands/template-parts/content/entry_loop.php
@@ -11,7 +11,7 @@ namespace WP_Rig\WP_Rig;
 $this_post_type = get_post_type();
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry loop-entry' ); ?>>
 	<?php
 	get_template_part( 'template-parts/content/entry_header', $this_post_type );
 	get_template_part( 'template-parts/content/entry_summary', $this_post_type );
