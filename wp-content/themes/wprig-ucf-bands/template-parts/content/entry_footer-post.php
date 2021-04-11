92a8576920517ea91dc1e3dc4d1be10f4adc86f8
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-10 22:38:48 -0400

    use general .wrap class for with + max with auto margin centering

--- wp-content/themes/wprig-ucf-bands/template-parts/content/entry_footer-post.php
+++ wp-content/themes/wprig-ucf-bands/template-parts/content/entry_footer-post.php
@@ -9,6 +9,6 @@
 namespace WP_Rig\WP_Rig;
 
 ?>
-<footer class="entry-footer">
+<footer class="wrap entry-footer">
 	<?php get_template_part( 'template-parts/content/entry_actions', get_post_type() ); ?>
 </footer>
