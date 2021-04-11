d93a33c0d42acb0ac290d55520d7fc11b0d991ed
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-11 18:11:44 -0400

    Adjust post navigation template for new theming

--- wp-content/themes/wprig-ucf-bands/template-parts/content/entry.php
+++ wp-content/themes/wprig-ucf-bands/template-parts/content/entry.php
@@ -27,8 +27,8 @@ $this_post_type = get_post_type();
 if ( 'post' === $this_post_type || get_post_type_object( $this_post_type )->has_archive ) {
 	the_post_navigation(
 		[
-			'prev_text' => '<div class="post-navigation-sub"><span>' . esc_html__( 'Previous:', 'wp-rig' ) . '</span></div>%title',
-			'next_text' => '<div class="post-navigation-sub"><span>' . esc_html__( 'Next:', 'wp-rig' ) . '</span></div>%title',
+			'prev_text' => '<div class="post-navigation-sub"><span class="icon-link">' . esc_html__( 'Previous:', 'wp-rig' ) . '</span></div>%title',
+			'next_text' => '<div class="post-navigation-sub"><span class="icon-link">' . esc_html__( 'Next:', 'wp-rig' ) . '</span></div>%title',
 		]
 	);
 }
