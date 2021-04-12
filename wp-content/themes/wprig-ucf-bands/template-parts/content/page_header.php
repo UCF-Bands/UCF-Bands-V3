5d29a9413c391eb0556590a861662593a2bdc101
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-11 22:07:17 -0400

    Kill trailing page-header comments

--- wp-content/themes/wprig-ucf-bands/template-parts/content/page_header.php
+++ wp-content/themes/wprig-ucf-bands/template-parts/content/page_header.php
@@ -14,7 +14,7 @@ if ( is_404() ) {
 		<h1 class="page-title">
 			<?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp-rig' ); ?>
 		</h1>
-	</header><!-- .page-header -->
+	</header>
 	<?php
 } elseif ( is_home() && ! have_posts() ) {
 	?>
@@ -22,7 +22,7 @@ if ( is_404() ) {
 		<h1 class="page-title">
 			<?php esc_html_e( 'Nothing Found', 'wp-rig' ); ?>
 		</h1>
-	</header><!-- .page-header -->
+	</header>
 	<?php
 } elseif ( is_home() && ! is_front_page() ) {
 	get_template_part( 'template-parts/content/archive_cover' );
@@ -38,7 +38,7 @@ if ( is_404() ) {
 			);
 			?>
 		</h1>
-	</header><!-- .page-header -->
+	</header>
 	<?php
 } elseif ( is_archive() ) {
 	get_template_part( 'template-parts/content/archive_cover' );
