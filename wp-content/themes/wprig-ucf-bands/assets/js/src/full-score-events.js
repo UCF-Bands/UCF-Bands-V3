31e9c3fbce00c650c8a6e7981f91bb41f9d3ca6f
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-17 01:42:32 -0400

    Wait until document has loaded until initializing FSE stuff :(

--- wp-content/themes/wprig-ucf-bands/assets/js/src/full-score-events.js
+++ wp-content/themes/wprig-ucf-bands/assets/js/src/full-score-events.js
@@ -4,11 +4,18 @@
  * @since 3.0.0
  */
 
-initFSE();
+if ( 'loading' === document.readyState ) {
+	// The DOM has not yet been loaded.
+	document.addEventListener( 'DOMContentLoaded', initFSE );
+} else {
+	// The DOM has already been loaded.
+	initFSE();
+}
 
 function initFSE() {
 	// get all upcoming events blocks that are widely-aligned as a last child of
 	// a jumbo or banner cover.
+	// @todo the margin this adds (negatively) causis bad CLS!
 	document.querySelectorAll( `
 		.wp-block-cover.is-style-banner .fse-upcoming-events.alignwide:last-child,
 		.wp-block-cover.is-style-jumbo .fse-upcoming-events.alignwide:last-child
