c25a38f3e4a5cc0320324edc3c69633d9aac3a16
Jordan Pakrosnis <jpak@jordanpak.com>
2022-02-18 09:58:25 -0500

    Build out the dynamic link column functionality

--- wp-content/plugins/tablepress-dynamic-link-column/tablepress-dynamic-link-column.php
+++ wp-content/plugins/tablepress-dynamic-link-column/tablepress-dynamic-link-column.php
@@ -14,6 +14,11 @@
 
 namespace Tablepress_Dynamic_Link_Column;
 
+// Set constants
+define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_VERSION', '0.1.0' );
+define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_URL', plugin_dir_url( __FILE__ ) );
+
 /**
  * Plugin wrapper
  *
@@ -52,7 +57,6 @@ class Plugin {
 	 * @since 0.1.0
 	 */
 	public function __construct() {
-		$this->set_constants();
 		add_action( 'plugins_loaded', [ $this, 'init' ] );
 	}
 
@@ -62,18 +66,75 @@ class Plugin {
 	 * @since 0.1.0
 	 */
 	public function init() {
+		add_filter( 'tablepress_shortcode_table_default_shortcode_atts', [ $this, 'add_shortcode_atts' ] );
+		add_filter( 'tablepress_table_raw_render_data', [ $this, 'add_column' ], 10, 2 );
+
 		do_action( 'tablepress_dynamic_link_column_loaded' );
 	}
 
 	/**
-	 * Set constants
+	 * Register new attributes for [table /] shortcode
 	 *
 	 * @since 0.1.0
+	 *
+	 * @param  array $default_atts  Existing [table] shortcode attributes.
+	 * @return array $default_atts
 	 */
-	private function set_constants() {
-		define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_VERSION', '0.1.0' );
-		define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_DIR', plugin_dir_path( __FILE__ ) );
-		define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_URL', plugin_dir_url( __FILE__ ) );
+	public function add_shortcode_atts( $default_atts ) {
+		$default_atts['dynamic_link_column_source_column'] = false;
+		$default_atts['dynamic_link_column_text']          = '';
+		$default_atts['dynamic_link_column_param']         = '';
+		$default_atts['dynamic_link_column_url']           = '';
+		return $default_atts;
+	}
+
+	/**
+	 * Insert dynamic link column into table
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param  array $table           Table data and details.
+	 * @param  array $render_options  Table render options.
+	 * @return array $table
+	 */
+	public function add_column( $table, $render_options ) {
+
+		// Bounce if we don't have a source column to pull a value from.
+		if ( empty( $table['data'] ) || false === $render_options['dynamic_link_column_source_column'] ) {
+			return $table;
+		}
+
+		foreach ( $table['data'] as $index => &$row ) {
+
+			// Don't apply to column header or footer.
+			if (
+				( 0 === $index && true === $render_options['table_head'] )
+				|| ( count( $table['data'] ) - 1 === $index && true === $render_options['table_foot'] )
+			) {
+				array_push(
+					$row,
+					esc_html( $render_options['dynamic_link_column_text'] ?: __( 'Link', 'tablepress-dynamic-link-column' ) )
+				);
+				continue;
+			}
+
+			// Take the value of the designated column and insert it as the
+			// parameter in a link in a new column at the end.
+			array_push(
+				$row,
+				sprintf(
+					'<a class="tablepress-dynamic-link-column-link" href="%1$s">%2$s</a>',
+					add_query_arg(
+						urlencode( $render_options['dynamic_link_column_param'] ?: 'tp_dynamic_link' ),
+						urlencode( $row[ intval( $render_options['dynamic_link_column_source_column'] ) ] ),
+						esc_url( $render_options['dynamic_link_column_url'] ?: '#' )
+					),
+					esc_html( $render_options['dynamic_link_column_text'] ?: __( 'Link', 'tablepress-dynamic-link-column' ) )
+				)
+			);
+		}
+
+		return $table;
 	}
 }
 
