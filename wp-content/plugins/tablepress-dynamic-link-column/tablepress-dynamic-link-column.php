2a613770db74838e5e6121ca3095f21573ccb6ca
Jordan Pakrosnis <jpak@jordanpak.com>
2022-02-18 09:18:09 -0500

    Init main plugin class

--- wp-content/plugins/tablepress-dynamic-link-column/tablepress-dynamic-link-column.php
+++ wp-content/plugins/tablepress-dynamic-link-column/tablepress-dynamic-link-column.php
@@ -12,4 +12,81 @@
  * @package         Tablepress_Dynamic_Link_Column
  */
 
-// Your code starts here.
+namespace Tablepress_Dynamic_Link_Column;
+
+/**
+ * Plugin wrapper
+ *
+ * @since 0.1.0
+ */
+class Plugin {
+
+	/**
+	 * The single instance of this class
+	 *
+	 * @since 0.1.0
+	 * @var   Plugin
+	 */
+	protected static $instance;
+
+	/**
+	 * Get main plugin instance.
+	 *
+	 * @since 0.1.0
+	 * @see   instance()
+	 *
+	 * @return Plugin
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Spin up plugin
+	 *
+	 * @since 0.1.0
+	 */
+	public function __construct() {
+		$this->set_constants();
+		add_action( 'plugins_loaded', [ $this, 'init' ] );
+	}
+
+	/**
+	 * Hook in our functionality
+	 *
+	 * @since 0.1.0
+	 */
+	public function init() {
+		do_action( 'tablepress_dynamic_link_column_loaded' );
+	}
+
+	/**
+	 * Set constants
+	 *
+	 * @since 0.1.0
+	 */
+	private function set_constants() {
+		define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_VERSION', '0.1.0' );
+		define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_DIR', plugin_dir_path( __FILE__ ) );
+		define( 'TABLEPRESS_DYNAMIC_LINK_COLUMN_URL', plugin_dir_url( __FILE__ ) );
+	}
+}
+
+/**
+ * Get instance of main plugin class
+ *
+ * @since 0.1.0
+ *
+ * @return Plugin
+ */
+function instance() {
+	return Plugin::instance();
+}
+
+// Instantiate plugin wrapper class.
+$tablepress_dynamic_link_column = instance();
